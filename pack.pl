38f5a8893702a10cd2c567dcffd9fe59fd335fb5
Roy Ratcliffe <roy@ratcliffe.me>
2023-08-12 11:30:08 +0100

    feat: Prolog pack

---
+++ pack.pl
@@ -0,0 +1,8 @@
+name(mavlink).
+version('0.1.0').
+title('MAVLink for SWI-Prolog').
+author('Roy Ratcliffe', 'roy@ratcliffe.me').
+packager('Roy Ratcliffe', 'roy@ratcliffe.me').
+maintainer('Roy Ratcliffe', 'roy@ratcliffe.me').
+home('https://github.com/swipl/mavlink').
+download('https://github.com/swipl/mavlink/releases/*.zip').
