6477539251a885b80d5b2a19e758fbee2eb4f051
Roy Ratcliffe <roy@ratcliffe.me>
2023-08-26 07:48:50 +0100

    fix: brackets

--- prolog/mavlink/crc_16_mcrf4xx.pl
+++ prolog/mavlink/crc_16_mcrf4xx.pl
@@ -24,7 +24,7 @@ crc_16_mcrf4xx(Check0, Data, Check) :-
     integer(Data),
     !,
     Data_ is (Check0 /\ 16'FF) xor (Data /\ 16'FF),
-    Data__ is Data_ xor ((Data_ << 4) /\ 16'FF),
+    Data__ is (Data_ xor (Data_ << 4)) /\ 16'FF,
     Check is (Check0 >> 8) xor (Data__ << 8) xor (Data__ << 3) xor (Data__ >> 4).
 crc_16_mcrf4xx(Check0, Data, Check) :-
     is_list(Data),
